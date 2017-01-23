74a3fed791525c62156e62ecc594e3b61e14b1c6
Joseph Fifer <jfifer@inttools.coredial.net>
2017-01-23 21:26:47 +0000

    Fix router bug

--- js/controllers.js
+++ js/controllers.js
@@ -4,4 +4,5 @@ dialToneCtrl.controller('homeController', function ($scope, Portal) {
 	Portal.create({test:'yes'}, function(res) {
 		console.log(res);
 	});
+	Portal.get({test: 'no'}, function(res) { console.log(res); });
     });
